4cdb3383ccd49927991c17e4d3473de2400d4e0e
yujeongJeon <workingnewjeong@gmail.com>
2023-02-05 00:53:40 +0900

    🩹 example 컴포넌트 추가 및 작은 수정 - 스타일 수정 - 불필요한 header 값 제거

---
+++ apps/storybook/stories/Components/Example.stories.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { COLORS, TYPOS } from 'ui'
+import AutoSelectInput from '../../common/AutoSelectInput'
+import { Column, Row } from '../../common/Flex'
+
+export const Example = () => {
+    const [selectedNames, setSelectedName] = useState<string[]>([])
+    const list = ['newjeong', 'yujeong', 'yullia.jeon', 'juj', 'yujeong-jeon', 'yujeongJeon']
+    const onSearch = (nicknames: string[]) => {
+        setSelectedName(nicknames)
+    }
+    return (
+        <Column>
+            <Text>
+                {selectedNames.map((nickname) => (
+                    <Span key={nickname}>{nickname}</Span>
+                ))}
+            </Text>
+            <AutoSelectInput list={list} onSearch={onSearch} placeholder={'내 닉네임 검색'} />
+        </Column>
+    )
+}
+
+const Text = styled(Row)`
+    ${TYPOS.PRETENDARD_BODY1_MEDIUM}
+    color: ${COLORS.GRAYSCALE.GRAY_9};
+`
+
+const Span = styled.span`
+    margin: 0 4px;
+`
+
+export default {
+    title: "Bibi's Design System/Component/Example",
+    component: Example,
+}
